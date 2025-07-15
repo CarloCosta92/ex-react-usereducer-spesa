d43f404ff84e352e33b2ad0b8896d60b2fd793d4
Carlo Costa <carlo.costa92@gmail.com>
2025-07-15 22:44:15 +0200

    feat: solved milestone 2

--- ex-react-usereducer-spesa/src/components/Prodotti.jsx
+++ ex-react-usereducer-spesa/src/components/Prodotti.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const Prodotti = () => {
+    // Aggiungi uno stato locale addedProducts(inizialmente un array vuoto) per rappresentare i prodotti nel carrello.
+    const [addedProducts, setAddedProducts] = useState([]);
 
     const products = [
         { name: 'Mela', price: 0.5 },
@@ -6,6 +10,16 @@ const Prodotti = () => {
         { name: 'Latte', price: 1.0 },
         { name: 'Pasta', price: 0.7 },
     ];
+    // Per ogni prodotto della lista, aggiungi un bottone "Aggiungi al carrello":
+    // Al click del bottone, usa una funzione addToCart per:
+    // Aggiungere il prodotto al carrello se non è già presente, con una proprietà quantity = 1.
+    // Se il prodotto è già nel carrello, ignora l’azione.
+    const addToCart = (product) => {
+        const alreadyAdded = addedProducts.find(item => item.name === product.name);
+        if (!alreadyAdded) {
+            setAddedProducts([...addedProducts, { ...product, quantity: 1 }]);
+        }
+    }
 
     return (
         <>
@@ -16,12 +30,28 @@ const Prodotti = () => {
                         <div className="card-body" key={index}>
                             <h5 className="card-title">{el.name}</h5>
                             <p className="card-text"> Prezzo: {el.price}€</p>
+                            <button onClick={() => addToCart(el)} className="btn btn-primary">
+                                Aggiungi al carrello
+                            </button>
                         </div>
-                    )
-
-                    )}
+                    ))}
                 </ul>
             </div>
+
+            {
+                addedProducts.length > 0 && (
+                    <div className="mt-4">
+                        <h2>Prodotti nel carrello</h2>
+                        <ul className="list-group">
+                            {addedProducts.map((el, index) => (
+                                <li key={index} className="list-group-item">
+                                    <strong>{el.name}</strong> - {el.price} € - Quantità: {el.quantity}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )
+            }
         </>
     )
 
